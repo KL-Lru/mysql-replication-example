331e31082a6083bb3aeeec4991fd93d1530dd6c3
KL-Lru <kilattoeruru@outlook.jp>
2024-02-17 02:08:23 +0900

    fix サンプル用に挿入データ調整

--- gtid_base_replication/master/scripts/seed.sh
+++ gtid_base_replication/master/scripts/seed.sh
@@ -4,10 +4,12 @@ mysql -v << SQL
 USE test;
 
 CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT,
+  id VARCHAR(36) NOT NULL,
   name VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO users (name) VALUES ('John'), ('Jane'), ('Bob'), ('Alice'), ('Eve');
+INSERT INTO users (id, name) VALUES (UUID(), 'John'), (UUID(), 'Jane'), (UUID(), 'Bob'), (UUID(), 'Alice'), (UUID(), 'Eve');
+
+UPDATE users SET name = CONCAT(name, ' Doe') WHERE name = 'John' OR name = 'Jane';
 SQL
